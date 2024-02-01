95883ad44ff13ba9e624443a56328ee1dd43c66f
Vadim <119964682+RustyFrukt@users.noreply.github.com>
2024-02-01 11:24:25 +0300

    Delete

--- task_2/HealthyFood.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.seminar_1.task_2;
-
-/**
- * Здоровая еда
- */
-public interface HealthyFood extends Food{
-}
--- task_2/SemiFinishedFood.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.seminar_1.task_2;
-
-/**
- * Полуфабрикат
- */
-public interface SemiFinishedFood extends Food{
-}
-
